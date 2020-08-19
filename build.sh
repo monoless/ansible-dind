d61f281e806da82565f438126ae60fb51a33abc5
Hyun Seok Li <ecst@monolife.co.kr>
2020-08-19 11:41:24 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker build -t ansible-docker .
-docker tag ansible-docker monoless/ansible-docker:18.09.9
+docker tag ansible-docker monoless/ansible-docker:18.09.9-dind
 docker login
-docker push monoless/ansible-docker:18.09.9
+docker push monoless/ansible-docker:18.09.9-dind
 docker logout
